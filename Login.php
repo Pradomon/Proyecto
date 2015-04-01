0110b3132f662a44a226b21f73ecc5f9d6085574
Prado Monteagudo <prado.monteagudo.jimenez@gmail.com>
2015-04-01 13:42:14 +0200

    01-04-2015
    Copia Miercoles 01-04-2015

--- Login.php
+++ Login.php
@@ -1,31 +1,35 @@
-<html>
-<head>
-	<title>LOGIN</title>
-</head>
+<!DOCTYPE html>
+<html lang="es"> 
+<head><!-- Index -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<title> LOGIN </title>
+	<!--                                                                                 PROPIOS    -->
+	<link href="css/cocina.css" 		rel="stylesheet" type="text/css" media="screen" > 
+	<!--                                                                                 ADAPTACION -->
+	
+
+	<link rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap.min.css">
+	<script type="text/javascript" src="js/cocina.js"></script>
+  	
+
+</head>	
+
 <body>
 
 <?php
 
 	$nomusu = $_POST["nomusu"];
 	$password1 = $_POST["password1"];
-	$correo1 = $_POST["correo1"];
+	
 
-//                       INSERTAMOS EN LA BD
-
-$ok = selectBD($nomusu, $password1);
-	if($ok)
-		echo "<p>Datos leidos correctamente</p>";
-	else
-		echo "<p>Error en la lectura de datos</p>";
-
-
-	echo "<p>Fecha nacimiento : $fecha </p>";
-
-//                       INSERTAMOS EN LA BD
-
-function selectBD($nomusu, $password1)
-{
-//guardar los datos en la BD
+	echo $nomusu;
+	echo "<p></p>";
+	echo $password1;
+	echo "<p></p>";
+	
+	
+	//    CONEXION A LA BD
 	$link = mysqli_connect(
 		'localhost', // El servidor
 		'usuweb', // El usuario
@@ -41,25 +45,37 @@ function selectBD($nomusu, $password1)
 		return false;
 	}
 
-	$query = "SELECT INTO" 
-		. " usuarios (nomusu, email, fnacimiento)"
-		. " WHERE (nomusu='Pradomon')";
 	
-	echo "<p>$query</p>";
+//                       LEEMOS DE LA BD
 
-	$resultado = mysqli_query($link, $query);
-	if(!$resultado) 
-	{
-		echo "<p>Error al ejecutar la sentencia <b>$query</b>: " 
-		. mysqli_error($link)
-		. "</p>";
-		return false;
-	}
+//$query = "SELECT INTO" 
+//		. " usuarios (nomusu, email, fnacimiento)"
+//		. " WHERE (nomusu='Pradomon')";
 
-	return true;
+	
+//$query="SELECT nomusu, email, fnacimiento FROM usuarios WHERE nomusu = '$nomusu'";	
+//$query="SELECT * FROM usuarios WHERE codusu > 1";	
+$query = "SELECT * FROM usuarios";	
+$resultado = mysql_query($link,$query);
+
+if (!$resultado) 
+{
+    echo 'No se pudo ejecutar la consulta: ' . mysql_error();
+    echo "<p></p>";
+    echo 'error : ' . $resultado;
+    exit;
 }
 
+$posts = array();
+while ($row = mysql_fetch_assoc($resultado)) {
+    $posts[] = $row;
+}
 
+$fila = mysql_fetch_row($resultado);
+
+echo $fila[0]; // nomusu
+echo $fila[1]; // email
+echo $fila[2]; // fecha nacimiento
 
 ?>
 
